be9d6593373601baa3ff5e1135914383b6789fe6
Emily Baker-King <ebakerking@gmail.com>
2019-11-04 13:51:13 -0600

    Added list of quotes. Set up the quoteBox component to use quote props passed in. Added functionality to generate and show a new random quote

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 import QuoteBox from './QuoteBox';
+import quotes from './quotes';
 
 class App extends Component {
+  //Setting the constructor function so we can set our default state
+  constructor(props) {
+    super(props);
+    //set the first quote and author as our default state
+    this.state = {
+      quote: quotes[0].quote,
+      author: quotes[0].author
+    }
+  }
+
+  //create a function called fetRandmQuote that returns a random quote rom the array of quotes. Hint: You'll need to generate a random number between 0 and 1 less than the length of the array
+  getRandomQuote() {
+    const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomQuoteIndex];
+  }
+
+  //function that will be called when the new quote button on the quote box function is pressed
+  handleClick = () => {
+    //generate a random quote
+    const randomQuote = this.r=this.getRandomQuote();
+    //update our state with the information from the random quote
+    this.setState({
+      quote: randomQuote.quote,
+      author: randomQuote.author
+    });
+  }
+
   render() {
     return (
       <div>
-        <QuoteBox />
+        <QuoteBox
+          quote={this.state.quote}
+          author={this.state.author}
+          handleClick={this.handleClick}
+        />
       </div>
     );
   }
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -5,10 +5,12 @@ class QuoteBox extends Component {
     render() {
         return(
             <div>
-                <h1>Hello world</h1>
+                <h1>{this.props.quote}</h1>
+                <h1>{this.props.author}</h1>
+                <button onClick={this.props.handleClick}>New Quote</button>
             </div>
         );
     }
 }
 
-export default QuoteBo;
+export default QuoteBox;
