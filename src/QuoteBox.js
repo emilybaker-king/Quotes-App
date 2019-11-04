c556ea48378c4293be06058d18018a7010670ffe
Emily Baker-King <ebakerking@gmail.com>
2019-11-04 12:43:16 -0600

    removed boilerplate content from app.js. created the quotebox.js component and conected quotebox in app.js

---
+++ src/QuoteBox.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class QuoteBox extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Hello world</h1>
+            </div>
+        );
+    }
+}
+
+export default QuoteBo;
